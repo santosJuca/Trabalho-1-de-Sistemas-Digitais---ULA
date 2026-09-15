6efd570720d60dacf721eb5464415e399aed2c27
Állan Viana da Rosa <allanvrosa979@gmail.com>
2026-09-15 08:49:55 -0300

    arquivos full_adder.sv e half_adder.sv alteração
    Segue Os dois arquivos half adder reutilizado e full adder implementado

---
+++ full_adder.sv
@@ -0,0 +1,30 @@
+//Autor: allan 
+module full_adder (
+    input  logic a,
+    input  logic b,
+    input  logic cin,
+    output logic sum,
+    output logic cout
+);
+
+    logic s1;
+    logic c1;
+    logic c2;
+
+    half_adder ha0 (
+        .a(a),
+        .b(b),
+        .sum(s1),
+        .carry(c1)
+    );
+
+    half_adder ha1 (
+        .a(s1),
+        .b(cin),
+        .sum(sum),
+        .carry(c2)
+    );
+
+    assign cout = c1 | c2;
+
+endmodule
